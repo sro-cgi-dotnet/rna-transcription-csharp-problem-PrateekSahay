6e93a25c7cb588267590cb3cda5a4c03756ff3d4
Prateek Sahay <prateeksahay96@gmail.com>
2018-09-16 14:24:37 +0530

    Using StringBuilder

--- RNATranscription/RNATranscription.cs
+++ RNATranscription/RNATranscription.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Text;
 
 namespace RNATranscriptionModule
 {
     public static class RnaTranscription
     {
-        public static string ToRna(string nucleotide)
-        {
+        public static string ToRna(string nucleotide){
+            /* 
             string rna="";
             foreach(char nuc in nucleotide){
                  switch(nuc){
@@ -24,7 +25,30 @@ namespace RNATranscriptionModule
                    
                 }
             }
+            
             return rna;
+        }
+        */
+        //Using StringBuilder
+
+        StringBuilder rna = new StringBuilder();
+            foreach(char nuc in nucleotide){
+                switch(nuc){
+                    case 'G':
+                            rna.Append("C");
+                            break;
+                    case 'C':
+                            rna.Append("G");
+                            break;
+                    case 'T':
+                            rna.Append("A");
+                            break;
+                    case 'A':
+                            rna.Append("U");
+                            break;
+                }
+            }
+            return rna.ToString();
         }
     }
 }
